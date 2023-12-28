1911d75a0ca8f7dccccd652ed253f57adbadb73f
SergiumarK <s.marcocimd@gmail.com>
2023-12-28 17:08:41 +0300

    filter-by_brand/price

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -28,53 +28,70 @@ const Cart = () => {
 
   return (
     <div>
-      <h1 className='text-2xl font-semibold'>Cart</h1>
-      <div className='grid grid-cols-3 '>
-        <div className='col-span-2 flex flex-col gap-2'>
-        {
-          cart.map(obj => (
-            <div className=' grid grid-cols-5 p-2 rounded justify-items-center items-center' key={obj.id}>
-              <img className='h-28 rounded-md ' src={obj.image} alt="Product image" />
-              <h1 className='font-semibold text-lg'>{obj.brand} {obj.model}</h1>
-              <p>
-                {
-                  obj.quantity > 1 ?
-                  `${obj.quantity} x ${obj.price} MDLb= ${obj.quantity * obj.price}` :
-                  `${obj.price} MDL`
-                }
-              </p>
-              <div className='flex gap-2 items-center justify-center'>
-                <button onClick={() => changeQnt(obj.id, "minus")} className={`h-10 w-10 text-lg bg-red-500 text-white rounded-full border-2 border-red-600 ${obj.quantity <= 1 ? "hidden" : ""} `}>-</button>
-                <h2 className='text-2xl font-semibold'>{obj.quantity}</h2>
-                <button onClick={() => changeQnt(obj.id, "plus")} className='h-10 w-10 text-lg bg-green-500 text-white rounded-full border-2 border-green-600'>+</button>
+      {
+        cart.length > 0 ?
+        (
+          <>
+            <div className='flex justify-between py-3 items-center'>
+              <h1 className='text-2xl font-semibold'>Cart</h1>
+              <button onClick={() => setCart([])} className="flex gap-2 itmes-center bg-blue-600 text-white py-1 px-2 rounded-md hover:bg-blue-700 active:bg-blue-800 items-baseline">Clear</button>
+            </div>
+        
+            <div className='grid grid-cols-3 gap-3'>
+              <div className='col-span-2 flex flex-col gap-2'>
+              {
+                cart.map(obj => (
+                  <div className='cart_product border border-slate-300 grid p-2 rounded justify-items-center items-center' key={obj.id}>
+                    <img className='h-28 rounded-md ' src={obj.image} alt="Product image" />
+                    <h1 className='font-semibold text-lg'>{obj.brand} {obj.model}</h1>
+                    <p>
+                      {
+                        obj.quantity > 1 ?
+                        `${obj.quantity} x ${obj.price} MDLb= ${obj.quantity * obj.price}` :
+                        `${obj.price} MDL`
+                      }
+                    </p>
+                    <div className='flex gap-2 items-center justify-center'>
+                      <button onClick={() => changeQnt(obj.id, "minus")} className={`h-10 w-10 text-lg bg-red-500 text-white rounded-full border-2 border-red-600 ${obj.quantity <= 1 ? "hidden" : ""} `}>-</button>
+                      <h2 className='text-2xl font-semibold'>{obj.quantity}</h2>
+                      <button onClick={() => changeQnt(obj.id, "plus")} className='h-10 w-10 text-lg bg-green-500 text-white rounded-full border-2 border-green-600'>+</button>
+                    </div>
+                    <div>
+                      <button onClick={() => deleteProduct(obj.id)} className='text-xl text-white rounded-full bg-gradient-to-r from-red-600 to-red-800 px-3 py-2 flex gap-2 items-center justify-center'>Delete <FaRegTrashAlt /></button>
+                    </div>
+                  </div>
+                ))
+              }
               </div>
-              <div>
-                <button onClick={() => deleteProduct(obj.id)} className='text-xl text-white rounded-full bg-gradient-to-r from-red-600 to-red-800 px-3 py-2 flex gap-2 items-center justify-center'>Delete <FaRegTrashAlt /></button>
+
+              <div className='border border-slate-300 rounded p-2 flex flex-col gap-3'>
+                <h1 className='text-2xl font-semibold'>Total</h1>
+                <div className='flex flex-col gap-2'>
+                  {
+                    cart.map(obj => (
+                      <div className='flex justify-between items-center' key={obj.id}>
+                        <h1 className='text-lg font-medium'>{obj.brand} {obj.model}</h1>
+                        <p className='text-lg'>{obj.quantity} x {obj.price} MDL = {obj.quantity * obj.price} MDL</p>
+                      </div>
+                    ))
+                  }
+                </div>
+                <h1 className='text-xl font-semibold py-1 border-t border-gray-500'>
+                  {
+                    cart.reduce((sum, obj) => sum +(obj.quantity * obj.price), 0)
+                  }
+                  MDL
+                </h1>
               </div>
             </div>
-          ))
-        }
-        </div>
-        <div>
-          <h1>Total</h1>
-          <div>
-            {
-              cart.map(obj => (
-                <div>
-                  <h1>{obj.brand} {obj.model}</h1>
-                  <p>{obj.quantity} x {obj.price} MDL = {obj.quantity * obj.price} MDL</p>
-                </div>
-              ))
-            }
-          </div>
-          <h1>
-            {
-              cart.reduce((sum, obj) => sum +(obj.quantity * obj.price), 0)
-            }
-            MDL
-          </h1>
-        </div>
-      </div>
+          </>
+        )
+        :
+        (
+          <h1 className='text-xl font-semibold'>Empty cart</h1>
+        )
+      }
+      
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,75 @@
-import React, {useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { Ctx } from '../data/Context'
 import { Link } from 'react-router-dom' //pentru a putea naviga pe alta pagina folosind router
 import { FaShoppingCart } from "react-icons/fa";
 
 const Home = () => {
   const {data, addToCart} = useContext(Ctx)
+  const brandFilters = ["Apple", "Xiaomi", "Samsung"]
+  const [brandFilter, setBrandFilter] = useState("")
+  const [products, setProducts] = useState(data)
+  const [priceFilter, setPriceFilter] = useState({
+    min: 0,
+    max: 100000
+  })
+
+  useEffect(() => {
+    // avem nevoie de un tablou dinamic care va inlocui tabloul de data
+    if (brandFilter !== "") {
+      setProducts(data.filter(pr => pr.brand === brandFilter))
+    }
+  }, [brandFilter])
+
+  useEffect(() => {
+    if (priceFilter.min !== 0 || priceFilter.max !== 0) {
+      setProducts(data.filter(pr => pr.price >= priceFilter.min && pr.price <= priceFilter.max))
+
+      if (brandFilter !== "") {
+        setProducts(data.filter(pr => pr.price >= priceFilter.min && pr.price <= priceFilter.max && pr.brand === brandFilter))
+      }
+    }
+  }, [priceFilter])
 
   return (
     <div>
+      {/* filter by brand */}
+      <div className='flex my-2 gap-5'>
+        <div className='flex flex-col gap-1'>
+          <label className='text-lg font-medium' htmlFor="brandFilter">Filter by brand</label>
+          <select value={brandFilter} onChange={(e) => setBrandFilter(e.target.value)} className='p-2 border border-gray-300 bg-gray-100 rounded' id='brandFilter'>
+            {
+              brandFilters.map(filter => <option value={filter}>{filter}</option>)
+            }
+          </select>
+        </div>
+
+        {/* filter by min price */}
+        <div className="flex flex-col gap-1">
+          <label className="text-lg font-medium" htmlFor="minPriceFilter">Filter by min price</label>
+          <input type="number" className="p-2 border border-gray-300 bg-gray-100 rounded" placeholder="Min Price" value={priceFilter.min} onChange={(e) => setPriceFilter({...priceFilter, min: e.target.value })} />
+        </div>
+
+        {/* filter by max price */}
+        <div className="flex flex-col gap-1">
+          <label className="text-lg font-medium" htmlFor="minPriceFilter">Filter by max price</label>
+          <input type="number" className="p-2 border border-gray-300 bg-gray-100 rounded" placeholder="Max Price" value={priceFilter.max} onChange={(e) => setPriceFilter({...priceFilter, max: e.target.value})} />
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label className='text-lg font-medium' htmlFor="brandFilter">Filter by brand</label>
+          <select value={brandFilter} onChange={(e) => setBrandFilter(e.target.value)} className='p-2 border border-gray-300 bg-gray-100 rounded' id='brandFilter'>
+            {
+              brandFilters.map(filter => <option value={filter}>{filter}</option>)
+            }
+          </select>
+        </div>
+        <div className='flex flex-col justify-end'>
+          <button onClick={() => setProducts(data)} className='bg-black text-white px-3 py-1 flex items-center gap-2 text-lg rounded-md hover:bg-blue-700'>Reset</button>
+        </div>
+      </div>
       <div className='flex gap-4'>
         {
-          data.map(obj => (
+          products.map(obj => (
             <div className="h-auto w-64 bg-grey-100 border border-grey-200 rounded overflow-hidden p-2 hover:border-blue-600 hover:shadow-lg" key={obj.id}>
               <img  src={obj.image} alt={obj.id} />
               <div className='p-1 flex flex-col gap-1'>
