5e761783e2d27c31502b4ae481be875326d69fb7
SergiumarK <s.marcocimd@gmail.com>
2023-12-28 12:53:14 +0300

    cart_localStorage/useEffect

--- src/data/Context.jsx
+++ src/data/Context.jsx
@@ -1,12 +1,16 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 import dataJSON from "./Data.json"
+import { json } from "react-router-dom";
 
 export const Ctx = createContext()
 
 const Context = ({children}) => {
     const [data, setData] = useState(dataJSON)
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || [])
 
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }, [cart])
 
     const addToCart = (id) => {
         const product = data.find(obj => obj.id === id);
@@ -23,8 +27,8 @@ const Context = ({children}) => {
         } else {
             setCart([...cart, {...product, quantity: 1}])
         }
-
-        console.log(cart)
+        // crearea unei valori in localStorage cu denumirea cart si valloarea de tip string al tabloului cart
+        
     }
 
     return (
